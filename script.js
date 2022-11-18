045de8fa3eff633b31758ffab63b7b3e2a783359
endulum <apheum@zoho.com>
2022-11-18 14:29:02 -0800

    button activity and status text activity

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let playerScore = 0;
 let computerScore = 0;
 
+// game logic
+
 function getRandomChoice() {
     let x = Math.floor(Math.random()*3 + 1);
     switch (x) {
@@ -16,33 +18,45 @@ function playRound(playerChoice, computerChoice) {
         case ("rock"): switch (computerChoice) {
             case ("paper"): 
                 computerScore++;
+                document.getElementById('computerScore').innerHTML = computerScore;
                 return "You lose! Paper beats rock!";
             case ("scissors"): 
                 playerScore++;
+                document.getElementById('playerScore').innerHTML = playerScore;
                 return "You win! Rock beats scissors!";
         }; case ("paper"): switch (computerChoice) {
             case ("scissors"): 
                 computerScore++;
+                document.getElementById('computerScore').innerHTML = computerScore;
                 return "You lose! Scissors beats paper!";
             case ("rock"): 
                 playerScore++;
+                document.getElementById('playerScore').innerHTML = playerScore;
                 return "You win! Paper beats rock!";
         }; case ("scissors"): switch (computerChoice) {
             case ("rock"): 
                 computerScore++;
+                document.getElementById('computerScore').innerHTML = computerScore;
                 return "You lose! Rock beats scissors!";
             case ("paper"): 
                 playerScore++;
+                document.getElementById('playerScore').innerHTML = playerScore;
                 return "You win! Scissors beats paper!";
         };
     }
 }
 
-// to test ui
+// ui interaction
 
-document.getElementById('rock').addEventListener("click", () => 
-    console.log(playRound("rock", getRandomChoice())));
-document.getElementById('paper').addEventListener("click", () => 
-    console.log(playRound("paper", getRandomChoice())));
-document.getElementById('scissors').addEventListener("click", () => 
-    console.log(playRound("scissors", getRandomChoice())));
+const roundResult = document.getElementById('roundResult');
+const roundStatus = document.getElementById('roundStatus');
+
+document.getElementById('rock').addEventListener("click", () => choose("rock"));
+document.getElementById('paper').addEventListener("click", () => choose("paper"));
+document.getElementById('scissors').addEventListener("click", () => choose("scissors"));
+
+function choose(choice) {
+    let computerChoice = getRandomChoice();
+    roundResult.textContent = playRound(choice, computerChoice);
+    roundStatus.style.display = "none";
+}
